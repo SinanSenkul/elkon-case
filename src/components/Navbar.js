3a3632ff7d267e266cca33afbe842cf7f0fb1075
sinan senkul <sinan@sinan-senkul.local>
2023-08-21 07:45:42 +0300

    added sticky navbar on scroll up and refined minor details

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import elkonlogo from '../elkonlogo.png';
 import '../styles/navbar.css';
 import searchicon from '../searchicon.png';
+import searchicon_black from '../searchicon_black.png';
 
 export default function Navbar() {
+  var [lastScroll, setLastScroll] = useState(0);
+  var [scrolledUp, setScrolledUp] = useState(false);
+
+  function handleScroll() {
+    let currentScroll = window.scrollY;
+    if (currentScroll - lastScroll > 0 || currentScroll === 0) {
+      setScrolledUp(false);
+    }
+    else {
+      setScrolledUp(true);
+    }
+    setLastScroll(currentScroll);
+  }
+
+  window.addEventListener('scroll', handleScroll)
+
   return (
-    <div id="navbar">
+    <div id="navbar" className={scrolledUp ? "navbar_scrolledUp" : "navbar_scrolledDown"}>
       <div id="logocontainer">
         <img src={elkonlogo} alt="elkonlogo" id="logo"></img>
       </div>
       <div id="linkcontainer">
-        <a href="#" className="tag">Corporate</a>
-        <a href="#" className="tag">Products</a>
-        <a href="#" className="tag">References</a>
-        <a href="#" className="tag">Sustainability</a>
-        <a href="#" className="tag">Media</a>
-        <a href="#" className="tag">Career</a>
-        <a href="#" className="tag">Contact</a>
+        <a href="#" className={scrolledUp ? "tag_scrolledUp" : "tag_scrolledDown"}>Corporate</a>
+        <a href="#" className={scrolledUp ? "tag_scrolledUp" : "tag_scrolledDown"}>Products</a>
+        <a href="#" className={scrolledUp ? "tag_scrolledUp" : "tag_scrolledDown"}>References</a>
+        <a href="#" className={scrolledUp ? "tag_scrolledUp" : "tag_scrolledDown"}>Sustainability</a>
+        <a href="#" className={scrolledUp ? "tag_scrolledUp" : "tag_scrolledDown"}>Media</a>
+        <a href="#" className={scrolledUp ? "tag_scrolledUp" : "tag_scrolledDown"}>Career</a>
+        <a href="#" className={scrolledUp ? "tag_scrolledUp" : "tag_scrolledDown"}>Contact</a>
         <div id="selectcontainer">
           <select id="select" className="select" defaultValue="english">
             <option className="option" lang="en" value="english" selected>EN</option>
@@ -25,7 +42,12 @@ export default function Navbar() {
           </select>
         </div>
         <a href="#" id="searchbutton" className="searchbutton">
-          <img src={searchicon} alt="searchicon" className="searchicon"></img>
+          {!scrolledUp &&
+            <img src={searchicon} alt="searchicon" className="searchicon"></img>
+          }
+          {scrolledUp &&
+            <img src={searchicon_black} alt="searchicon" className="searchicon"></img>
+          }
         </a>
       </div>
     </div>
